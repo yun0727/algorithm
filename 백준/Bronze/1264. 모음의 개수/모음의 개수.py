be117fbaa326da9369c0201297b74e8a9dd8561f
yun0727 <121007206+yun0727@users.noreply.github.com>
2023-01-16 16:27:12 +0900

    [Bronze IV] Title: 모음의 개수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,4 @@
+string=input()
+while string!="#":
+    print(string.count("a")+string.count("A")+string.count("E")+string.count("I")+string.count("O")+string.count("U")+string.count("i")+string.count("e")+string.count("o")+string.count("u"))
+    string=input()
